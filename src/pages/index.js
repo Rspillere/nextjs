bbd7b6d3db9229bf78ded8bdcc64a04966d9bdac
Rspillere <rspillere@gmail.com>
2022-07-20 14:18:39 -0300

    meta chanche

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import ProductList from '@containers/ProductList';
 export default function Home() {
   return (
     <>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+    
       <Head>
         <title>React Shop</title>
       </Head>
